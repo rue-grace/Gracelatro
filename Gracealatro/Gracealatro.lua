2d292ac6ac5cd76537ecc198baad91231e806c22
Rue <ruegreacefan10@gmail.com>
2026-03-29 17:19:24 -0300

    Delete Gracealatro/Gracealatro.lua

--- Gracealatro/Gracealatro.lua
+++
@@ -1,9 +0,0 @@
-SampleJimbos = {}
-
-assert(SMODS.load_file("globals.lua"))()
-
--- Jokers
-local joker_src = NFS.getDirectoryItems(SMODS.current_mod.path .. "jokers")
-for _, file in ipairs(joker_src) do
-    assert(SMODS.load_file("jokers/" .. file))()
-end
